fff6e3cd6fa9d97e688c8e678e2846ebdff8a4c7
Davoyan <chikiti@ya.ru>
2025-10-03 10:02:23 +0300

    Update maxmind reader logic

--- services/maxmind.py
+++ services/maxmind.py
@@ -1,31 +1,44 @@
 import geoip2.database
 import asyncio
 
-from config.variables import MAXMIND_DB_CITY
-from config.variables import MAXMIND_DB_ASN
+from config.variables import MAXMIND_DB_CITY, MAXMIND_DB_ASN
+from functools import partial
 
 async def get_maxmind_info(ip: str) -> dict:
     info = {}
-    try:
-        with geoip2.database.Reader(MAXMIND_DB_CITY) as city_reader:
-            city = city_reader.city(ip)
-            info.update({
+    loop = asyncio.get_running_loop()
+
+    def read_city(ip):
+        with geoip2.database.Reader(MAXMIND_DB_CITY) as reader:
+            city = reader.city(ip)
+            return {
                 "country": city.country.iso_code,
                 "country_name": city.country.name,
                 "region": city.subdivisions.most_specific.name,
                 "city": city.city.name,
-            })
-    except Exception as e:
-        info["city_error"] = str(e)
+            }
 
-    try:
-        with geoip2.database.Reader(MAXMIND_DB_ASN) as asn_reader:
-            asn = asn_reader.asn(ip)
-            info.update({
+    def read_asn(ip):
+        with geoip2.database.Reader(MAXMIND_DB_ASN) as reader:
+            asn = reader.asn(ip)
+            return {
                 "asn_number": asn.autonomous_system_number,
                 "asn_org": asn.autonomous_system_organization,
-            })
-    except Exception as e:
-        info["asn_error"] = str(e)
-    
+            }
+
+    city_task = loop.run_in_executor(None, partial(read_city, ip))
+    asn_task = loop.run_in_executor(None, partial(read_asn, ip))
+
+    results = await asyncio.gather(city_task, asn_task, return_exceptions=True)
+
+    if isinstance(results[0], Exception):
+        info["city_error"] = str(results[0])
+    else:
+        info.update(results[0])
+
+    if isinstance(results[1], Exception):
+        info["asn_error"] = str(results[1])
+    else:
+        info.update(results[1])
+
     return info
